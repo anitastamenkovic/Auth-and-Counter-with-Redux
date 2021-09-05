610f70c2ba06d3c8ea97b463e9929158c6340560
anitastamenkovic <anitastamenkovic19@gmail.com>
2021-09-05 16:44:48 +0200

    Initial Commit

---
+++ src/store/index.js
@@ -0,0 +1,44 @@
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "./counter";
+import authReducer from "./auth";
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+export default store;
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       // Action payloads
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+
+// export default store;
